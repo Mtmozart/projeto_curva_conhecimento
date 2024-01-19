582cc42e942f6f78b91700447548de9bd57b4b25
matheus mozart <mmsnborges@gmail.com>
2024-01-19 13:34:53 -0400

    conectado com elephant

--- src/config/dataSource.ts
+++ src/config/dataSource.ts
@@ -5,6 +5,7 @@ export const AppDataSource = new DataSource({
   type: "postgres",
   host: process.env.DB_SEVER,
   port: parseInt(process.env.DB_HOST, 10),
+  url: process.env.DB_URL,
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
