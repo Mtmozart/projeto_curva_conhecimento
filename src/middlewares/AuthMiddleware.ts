2ade7910e47484ba8011cc64ecbef7d5e15129ae
matheus mozart <mmsnborges@gmail.com>
2024-02-14 12:54:13 -0400

    tirei o um console

--- src/middlewares/AuthMiddleware.ts
+++ src/middlewares/AuthMiddleware.ts
@@ -24,7 +24,7 @@ export default class AuthMiddleware {
 
       next();
     } catch (error) {
-      console.error('Erro na verificação do token:', error);
+
       return res.status(500).json({ message: "Erro na verificação do token." });
     }
   }
